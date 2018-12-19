56e67534cb64f2f8696a676c8d3e8078e89598cc
Jared Pendergraft <jaredpdesigns@gmail.com>
2018-12-19 07:36:21 -1000

    Added media query for dark mode for Safari

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,14 @@ export default {
   --contrast: white;
   --highlight: hsl(var(--hue), 32%, 38%);
   --highlight-50: hsl(var(--hue), 32%, 75%);
+  @media (prefers-color-scheme: dark) {
+    --base: hsl(var(--hue), 4%, 75%);
+    --base-38: hsl(var(--hue), 4%, 38%);
+    --base-12: hsl(var(--hue), 4%, 12%);
+    --bg: hsl(var(--hue), 4%, 12%);
+    --contrast: hsl(var(--hue), 12%, 4%);
+    --highlight-50: hsl(var(--hue), 32%, 24%);
+  }
 }
 
 :root.themeDark {
